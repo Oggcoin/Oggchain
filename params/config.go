649a87f91aea1c23ec527f519d0399b54edf4595
OggUgg <tribe@oggcoin.org>
2026-05-27 10:15:06 +0300

    Update config.go
    ChainId: 770088

--- params/config.go
+++ params/config.go
@@ -33,7 +33,7 @@ var (
 var (
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainId:             big.NewInt(1987),
+		ChainId:             big.NewInt(70088),
 		HomesteadBlock:      big.NewInt(0),
 		DAOForkBlock:        nil,
 		DAOForkSupport:      false,
